56b1135ee1082ab20b968393650357dab7a7d7b2
mohasin <mohasinakonda39@gmail.com>
2022-05-24 06:28:51 +0600

    orders summary added.

--- index.js
+++ index.js
@@ -11,21 +11,50 @@ app.use(cors())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rqmun.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
 
 async function run() {
     try {
         await client.connect()
-        const partsCollection = client.db("electric-parts").collection("parts")
-        console.log('db connect')
+        const partsCollection = client.db("ElectricTools").collection("tools")
+        const orderCollection = client.db("ElectricTools").collection("orders")
+        app.get('/tools', async (req, res) => {
+            const query = {}
+            const tools = await partsCollection.find(query).toArray()
+            res.send(tools)
+        })
+        // insert orders
+        app.post('/orders', async (req, res) => {
+            const data = req.body
+            const { name, img, price, stock, quantity } = data
+            const doc = { name, img, price, stock, quantity, paid: false }
+            const orderComplete = await orderCollection.insertOne(doc)
+            res.send(orderComplete)
+
+        })
+        // get all all orders
+        app.get('/orders', async (req, res) => {
+
+            const orderComplete = await orderCollection.find().toArray()
+            res.send(orderComplete)
+
+        })
+        // get orders for specific email or user 
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email
+            const filter = { email: email }
+            const orderComplete = await orderCollection.find(filter).toArray()
+            res.send(orderComplete)
+
+        })
     } finally {
 
     }
 }
+
+app.get('/', (req, res) => {
+    res.send({ massage: 'success' })
+})
 
 run().catch(console.dir)
 
